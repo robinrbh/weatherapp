dc1216b9407165cca0345869391a26e7b35840d1
robinrbh <rhartog@gmail.com>
2021-12-18 00:23:51 +0100

    CSS changes

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,25 @@
 <template>
   <div id="app">
-    <div class="container" :class="backgroundImage">
-      <h1>Find your weather</h1>
-      <input 
-        placeholder="Typ een stad..."
-        v-model="query"
-        type="text" 
-        @keyup.enter="getWeather"
-      />
-      <p v-if="error">Deze stad is niet gevonden.</p>
+      <div class="container">
+        <h1>Vind het weer in jouw stad</h1>
+        <input 
+          placeholder="Typ een stad..."
+          v-model="query"
+          type="text" 
+          @keyup.enter="getWeather"
+        />
+        <p v-if="error">Deze stad is niet gevonden.</p>
 
-      <div class="weather-data" v-if="weather">
-      <!-- <Icon :name="weather.weather.description" /> -->
-        <h1>{{ weather.name }}</h1>
-        <div class="weather-left">
-          <p class="temp">{{ Math.round(weather.main.temp) }} C</p>
-        </div>
-        <div class="weather-right">
-          <inline-svg :src="require(`./assets/img/icons/${weather.weather[0].icon}.svg`)"></inline-svg> 
+        <div v-if="weather" class="weather-data">
+          <div class="weather-left" :class="backgroundImage">
+            <h2 class="city">{{ weather.name }}</h2>
+            <p class="temp">{{ Math.round(weather.main.temp) }} C</p>
+            <inline-svg class="icon" :src="require(`./assets/img/icons/${weather.weather[0].icon}.svg`)"></inline-svg> 
+          </div>
+          <div class="weather-right">
+          </div>
         </div>
       </div>
-
-    </div>
   </div>
 </template>
 
@@ -78,7 +76,10 @@ html, body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  background: #3A4D97;
+  color: white;
+  display: flex;
+  align-items: center;
 }
 
 .warm {
@@ -93,30 +94,71 @@ html, body {
 
 .container {
   width: 900px;
-  min-height: 650px;
-  justify-content: center;
+  min-height: 100vh;
   margin: 0 auto;
-  background-position: bottom;
-  transition: 0.4s;
+  align-items: center;
 }
 
 .weather-data {
   display: flex;
-  align-content: center;
   align-items: center;
   width: 100%;
+  margin-top: 50px;
 }
 
 .weather-left {
-  flex-grow: 1;
+  width: 40%;
+  min-height: 400px;
+  background-position: bottom;
+  transition: 0.4s;
+  border-radius: 25px;
+  text-align: left;
+  position: relative;
+  -webkit-box-shadow: 0 0 20px -10px rgba(0, 0, 0, 0.2);
+	box-shadow: 0 0 20px -10px rgba(0, 0, 0, 0.2);
+	-webkit-transform: scale(1.1) perspective(1500px) rotateY(10deg);
+	transform: scale(1.1) perspective(1500px) rotateY(10deg);
+  z-index: 9;
 }
 
 .weather-right {
-  flex-grow: 1;
+  width: 60%;
+  height: 300px;
+  background: rgba(0,0,0,0.5);
+  backdrop-filter: blur(5px);
+  border-radius: 0 15px 15px 0;
+}
+
+svg {
+  filter: drop-shadow( 1px 1px 1px rgba(0, 0, 0, .5));
+}
+
+h1, h2, h3, h4, p {
+  padding: 0;
+  margin: 0;
+  text-shadow: 1px 1px 1px rgba(0, 0, 0, .5);
+}
+
+h1 {
+  margin-top: 1em;
+}
+
+h2.city {
+  position: absolute;
+  left: 25px;
+  top: 25px;
 }
 
 p.temp {
   font-size: 60px;
+  position: absolute;
+  left: 25px;
+  bottom: 25px;
+}
+
+.icon {
+  position: absolute;
+  top: 40px;
 }
 
 input {
@@ -126,13 +168,7 @@ input {
   margin: 0 auto;
 }
 
-svg {
-  padding: 15px;
-  position: relative;
-  margin: 0 auto;
-}
-
 path {
-  fill: #3A4D97;
+  fill: white;
 }
 </style>

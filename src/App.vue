8618fb6648a059417d6bb517e094a98ffa61851a
robinrbh <rhartog@gmail.com>
2021-12-18 13:00:48 +0100

    Added forecast data

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,28 @@
 <template>
   <div id="app">
       <div class="container">
-        <h1>Vind het weer in jouw stad</h1>
-        <input 
-          placeholder="Typ een stad..."
-          v-model="query"
-          type="text" 
-          @keyup.enter="getWeather"
-        />
-        <p v-if="error">Deze stad is niet gevonden.</p>
-
         <div v-if="weather" class="weather-data">
           <div class="weather-left" :class="backgroundImage">
-            <h2 class="city">{{ weather.name }}</h2>
-            <p class="temp">{{ Math.round(weather.main.temp) }} C</p>
+            <h2 class="city">{{ weather.name }}, {{ weather.sys.country }}</h2>
+            <p class="temp">{{ weather.main.temp | round }}&deg;C</p>
             <inline-svg class="icon" :src="require(`./assets/img/icons/${weather.weather[0].icon}.svg`)"></inline-svg> 
           </div>
           <div class="weather-right">
+            <h1>Vind het weer in jouw stad</h1>
+            <input 
+              placeholder="Typ een stad..."
+              v-model="query"
+              type="text" 
+              @keyup.enter="getWeather(); getForecast()"
+            />
+            <p>Voelt aan als: {{ weather.main.feels_like | round }} graden</p>
+            <p>Minimum: {{ weather.main.temp_min | round }} graden</p>
+            <p>Maximum: {{ weather.main.temp_max | round }} graden</p>
+
+            <h3>Forecast</h3>
+            <div class="forecast" v-for="(day, i) in forecast" :key="i">
+              <p>{{ day.dt | round }}: {{ day.main.temp | round }}</p>
+            </div>
           </div>
         </div>
       </div>
@@ -30,13 +36,15 @@ export default {
   name: 'App',
   data() { 
     return {
-      query: "",
+      query: "Groningen",
       weather: "",
+      forecast: "",
       error: false
     }
   },
-  created() {
+  mounted() {
     this.getWeather()
+    this.getForecast()
   },
   methods: {
     getWeather() {
@@ -49,6 +57,23 @@ export default {
               this.error = false;
             }, 1000);
         })
+    },
+    getForecast() {
+      BackendService.fetchForecast(this.query)
+        .then((response) => {
+          this.forecast = response.data.list.slice(0, 5)
+        }, () => {
+          this.error = true
+            setTimeout(() => {
+              this.error = false;
+            }, 1000);
+        })
+    }
+  },
+  filters: {
+    round (value) {
+      if (!value) return ''
+      return Math.round(value)
     }
   },
   computed: {
@@ -97,6 +122,7 @@ html, body {
   min-height: 100vh;
   margin: 0 auto;
   align-items: center;
+  padding: 0 30px;
 }
 
 .weather-data {
@@ -107,7 +133,7 @@ html, body {
 }
 
 .weather-left {
-  width: 40%;
+  width: 45%;
   min-height: 400px;
   background-position: bottom;
   transition: 0.4s;
@@ -116,17 +142,19 @@ html, body {
   position: relative;
   -webkit-box-shadow: 0 0 20px -10px rgba(0, 0, 0, 0.2);
 	box-shadow: 0 0 20px -10px rgba(0, 0, 0, 0.2);
-	-webkit-transform: scale(1.1) perspective(1500px) rotateY(10deg);
-	transform: scale(1.1) perspective(1500px) rotateY(10deg);
+	-webkit-transform: scale(1.1) perspective(1500px) rotateY(20deg);
+	transform: scale(1.1) perspective(1500px) rotateY(20deg);
   z-index: 9;
 }
 
 .weather-right {
-  width: 60%;
+  width: 55%;
   height: 300px;
   background: rgba(0,0,0,0.5);
   backdrop-filter: blur(5px);
   border-radius: 0 15px 15px 0;
+  text-align: left;
+  padding: 25px;
 }
 
 svg {
@@ -137,10 +165,6 @@ h1, h2, h3, h4, p {
   padding: 0;
   margin: 0;
   text-shadow: 1px 1px 1px rgba(0, 0, 0, .5);
-}
-
-h1 {
-  margin-top: 1em;
 }
 
 h2.city {
@@ -162,13 +186,32 @@ p.temp {
 }
 
 input {
-  width: 60%;
-  padding: 15px;
-  border-radius: 15px;
-  margin: 0 auto;
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  box-sizing: border-box;
+  border-radius: 10px;
+}
+
+input:focus {
+  box-shadow: 0 0 20px -10px rgba(0, 0, 0, 0.2);
 }
 
 path {
   fill: white;
+}
+
+@media screen and (max-width: 600px) {
+  .weather-left {
+    width: 100%;
+    min-height: auto;
+    background-position: bottom;
+    transition: 0.4s;
+    -webkit-box-shadow: 0;
+    box-shadow: 0;
+    -webkit-transform: none;
+    transform: none;
+    z-index: 9;
+  }
 }
 </style>
--- src/services/BackendService.js
+++ src/services/BackendService.js
@@ -1,11 +1,26 @@
 import axios from "axios"
 
 const apiKey = "9f0c8c81dcbb5d905af5e5ef135981ab"
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&units=metric&`
+const apiUrl = `https://api.openweathermap.org/data/2.5/`
+
+const apiClient = axios.create({
+  baseURL: apiUrl,
+  params: {
+      appid: apiKey,
+      units: "metric"
+  }
+});
 
 export default {
   fetchWeather(query) {
-    return axios.get(apiUrl, {
+    return apiClient.get("/weather", {
+      params: {
+        q: query
+      }
+    })
+  }, 
+  fetchForecast(query) {
+    return apiClient.get("/forecast", {
       params: {
         q: query
       }
